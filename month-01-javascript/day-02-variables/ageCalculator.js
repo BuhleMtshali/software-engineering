b1d8f08555de6e5f9d33ca656def8ce582ad54f0
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-05 19:34:28 +0200

    Updated the age calculator

--- month-01-javascript/day-02-variables/ageCalculator.js
+++ month-01-javascript/day-02-variables/ageCalculator.js
@@ -12,6 +12,21 @@ while(calculateAgain.toLowerCase() === "yes"){
     let birthMonth = parseInt(prompt("Enter your Birth Month: "));
 
     let day = parseInt(prompt("What day were you born(1-31): "));
+
+    const birthDate = new Date(birthYear, birthMonth, day); //MONTHS START AT 0 IN JS
+
+    let age = now.getFullYear() - birthDate.getFullYear();
+    
+    const monthsDiff = now.getMonth() - birthDate.getMonth();
+
+    const dayDiff = now.getDay() - birthDate.getDay();
+
+
+    //CHECKING IF BIRTHDAY HAS PASSED
+    if(monthsDiff < 0 || (monthsDiff === 0 && dayDiff < 0)){
+        age--; //BIRTHDAY HASNT HAPPENED
+    }
+
    
     calculateAgain = prompt("Do you want to calculate again: ");
 }
