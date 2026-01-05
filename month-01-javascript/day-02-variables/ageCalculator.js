8c9a9f27d5f24acf068c9efbe7f97fdd03b52390
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-05 19:29:03 +0200

    updated the birthdate variables

--- month-01-javascript/day-02-variables/ageCalculator.js
+++ month-01-javascript/day-02-variables/ageCalculator.js
@@ -1,11 +1,18 @@
 const prompt = require("prompt-sync")();
+const now = new Date();
 
 let calculateAgain = "yes";
 
 while(calculateAgain.toLowerCase() === "yes"){
-    console.log("loop is running...")
 
+    let name = prompt("Enter your name: ");
 
+    let birthYear = parseInt(prompt("Enter your Birth year: "));
+
+    let birthMonth = parseInt(prompt("Enter your Birth Month: "));
+
+    let day = parseInt(prompt("What day were you born(1-31): "));
+   
     calculateAgain = prompt("Do you want to calculate again: ");
 }
 
