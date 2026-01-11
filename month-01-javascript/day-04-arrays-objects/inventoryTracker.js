4fc0b022efbc56175c266dba02a7c06caa455a0c
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-11 18:15:38 +0200

    updated the while loop

---
+++ month-01-javascript/day-04-arrays-objects/inventoryTracker.js
@@ -0,0 +1,17 @@
+const prompt = require("prompt-sync")();
+
+console.log("====== 🎰 WELCOME TO MY MINI INVENTORY📦 TRACKER 🐝 ======")
+let inventoryArray = [];
+let reRun = "yes";
+
+//STARTING THE MAIN WHILE LOOP
+while(reRun.toLowerCase() === "yes"){
+    console.log("loop running");
+
+
+
+    //CLOSING THE LOOP
+    reRun = prompt("🛍️ Would you like to make to make another transaction? (yes/no): ");
+}
+
+console.log("========= ⛳️ THANK YOU FOR TRYING MY INVENTORY TRACKER 🎲 ============")
