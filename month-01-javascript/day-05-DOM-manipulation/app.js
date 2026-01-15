8325655128a426f775d7d17cc62bb62591704a22
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-15 20:49:55 +0200

    updated the js file

--- month-01-javascript/day-05-DOM-manipulation/app.js
+++ month-01-javascript/day-05-DOM-manipulation/app.js
@@ -1,1 +1,7 @@
-alert("heello")
+//DEFINING VARIABLES
+let formInput = document.querySelector("#search-input");
+let form = document.querySelector("#form");
+
+form.addEventListener("click", (e) => {
+    e.preventDefault();
+})
