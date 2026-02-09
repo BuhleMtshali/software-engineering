73c47d940ecb9f0fb3af1cc5d0b9369e19248b6e
Buhle <mtshalizanobuhle7@gmail.com>
2026-02-09 20:36:53 +0200

    updated the app.js

--- month-01-javascript/day-06-event-forms/app.js
+++ month-01-javascript/day-06-event-forms/app.js
@@ -1,8 +1,15 @@
 //GETIING ALL THE BUTTONS
 const buttons = document.querySelectorAll(".rate-btn");
-buttons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-        e.preventDefault()
+
+//SUBMITTING THE FORM
+let form = document.querySelector("form")
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const fullName = document.querySelector("#fullName");
+    const email = document.querySelector("#emailAddress")
+    buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        
         //FIRST I NEED TO REMOVE THE ACTIVE CLASS
         buttons.forEach((btn) => {
             btn.classList.remove("active")
@@ -13,13 +20,7 @@ buttons.forEach((button) => {
 
         //LOGGING THE VALUE OF THE BTN
         console.log("Selected value: ", button.dataset.value)
+     })
     })
-})
-
-
-//SUBMITTING THE FORM
-let form = document.querySelector("form")
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    console.log("i submitted the form")
+    
 })
