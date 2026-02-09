85762a1571cabe0031b271ff7e992c62eff225f7
Buhle <mtshalizanobuhle7@gmail.com>
2026-02-09 20:15:29 +0200

    created the function to select the button

---
+++ month-01-javascript/day-06-event-forms/app.js
@@ -0,0 +1,16 @@
+//GETIING ALL THE BUTTONS
+const buttons = document.querySelectorAll(".rate-btn");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        //FIRST I NEED TO REMOVE THE ACTIVE CLASS
+        buttons.forEach((btn) => {
+            btn.classList.remove("active")
+        })
+
+        //ADDING THE ACTIVE CLASS
+        button.classList.add("active")
+
+        //LOGGING THE VALUE OF THE BTN
+        console.log("Selected value: ", button.dataset.value)
+    })
+})
