7785fd1971700dbace44f4539cbe979d5bc43fe7
Buhle <mtshalizanobuhle7@gmail.com>
2026-03-20 16:49:38 +0200

    created the method for deposit

--- month-01-javascript/day-08-scopes-closures/bankAccount.js
+++ month-01-javascript/day-08-scopes-closures/bankAccount.js
@@ -3,6 +3,17 @@ const prompt = require("prompt-sync")();
 
 //BANK FUNCTION
 function createBankAccount(){
+
     //this is the balance
     let balance = 0
+
+    //THIS IS WHERE I CREATE METHODS TO RETURN
+    return{
+
+        //DEPOSIT METHOD & AMOUNT
+        deposit(amount){
+            balance += amount;
+            console.log(`You deposited: R${amount} and the Balance is: R${balance}`)
+        }
+    }
 }
