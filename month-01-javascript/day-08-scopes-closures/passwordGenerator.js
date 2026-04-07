bf44b40fdf4ba48892a690c9ac4e114a2d283e15
Buhle <mtshalizanobuhle7@gmail.com>
2026-04-07 12:42:00 +0200

    created the main function

---
+++ month-01-javascript/day-08-scopes-closures/passwordGenerator.js
@@ -0,0 +1,7 @@
+//THIS IS THE OUTER FUNCTION
+function createPasswordGenerator(){
+    //THESE WILL THE PASSWORD CHARACTERS
+    const charSets = {
+        lower: 'abcdefghijklmnopqrstuvwxyz'
+    }
+}
