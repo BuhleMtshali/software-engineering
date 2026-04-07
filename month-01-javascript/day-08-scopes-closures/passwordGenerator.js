75d7e36dae3a27c17adadb73a48c12cf29a029c3
Buhle <mtshalizanobuhle7@gmail.com>
2026-04-07 12:59:58 +0200

    updated the variables

--- month-01-javascript/day-08-scopes-closures/passwordGenerator.js
+++ month-01-javascript/day-08-scopes-closures/passwordGenerator.js
@@ -2,6 +2,9 @@
 function createPasswordGenerator(){
     //THESE WILL THE PASSWORD CHARACTERS
     const charSets = {
-        lower: 'abcdefghijklmnopqrstuvwxyz'
+        lower: 'abcdefghijklmnopqrstuvwxyz',
+        upper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        nums: '0123456789',
+        special: '!@#$%^&*()-_=+[]{}|;:,.<>?'
     }
 }
