e1d86be136d5e7d7cd4d12f23e7f897fcb71d274
Buhle <mtshalizanobuhle7@gmail.com>
2026-04-14 13:53:32 +0200

    updated the grade analyzer

--- month-01-javascript/day-09-arrays-deep-dive/gradeAnalyser.js
+++ month-01-javascript/day-09-arrays-deep-dive/gradeAnalyser.js
@@ -4,3 +4,20 @@ console.log('===== 👩🏽‍🎓 MINI STUDENT GRADE ANALYZER🧺 =====')
 
 //this will hold the objects with grade objects
 let grades_students = []
+
+
+//THIS FUNCTION WILL ADD STUDENTS TO THE LIST
+function addStudent(studentArray){
+    let name = prompt("What is the student's name: ").toLowerCase()
+    let subject = prompt("What is the subject's name: ").toLowerCase()
+    let grade = parseInt(prompt("What is the Student's grade: "))
+    
+    //CREATING THE STUDENT OBJECT
+    let student_object = {
+        studentName: name,
+        studentSubject: subject,
+        studentGrade: grade
+    }
+
+    //NOW BEFORE ADDING THE STUDENTS LETS CHECK IF THEY EXIST
+}
